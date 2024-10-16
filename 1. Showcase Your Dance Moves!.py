7574116ad2f837ac1a2ed01594b1c862eba3f21b
KBSLimited <jdeandre1220@gmail.com>
2024-10-15 23:31:35 -0700

    Completed 1st Objective

---
+++ 1. Showcase Your Dance Moves!.py
@@ -0,0 +1,18 @@
+#Task 1: Code Correction
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#Task 2: Your Mood Today
+mood = input("How are you feeling today? Happy or sad?")
+
+if mood == "Happy":
+     print("Thats great to hear!")
+elif mood == "Sad":
+     print("I hope your day gets better!")
+else:
+    print("Silence is a loud answer, I'm always here for you buddy!")
